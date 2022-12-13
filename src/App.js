fbfc6245ef18ca97e2db0f006da5392400852ce7
shreyashbijlwanOP <shreyash.bijlwanmail@gmail.com>
2022-12-13 16:52:17 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Buttons from "./Components/AllStages/Buttons";
+import LineChart from "./Components/Graphs/LineChart";
+import Profile from "./Components/Profile/Profile";
+import Select from "./Components/Select.js/Select";
+import Funnel from "./Funnel";
+// import Funnel from "./Funnel";
 function App() {
   return (
     <div className="App">
+    <Select/>
+      <div className="box">
+      <Profile />
+      <Funnel />
+      </div>
+      <Buttons />
     
+      <LineChart/>
+     
     </div>
   );
 }
 
 export default App;
+
+/*
+ 
+*/
